01fb677c161834c9a7c4dd19ddfaac70c7cd6df3
PePeKirika <nongpepe@gmail.com>
2025-06-18 10:06:19 +0700

    temp

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+const address = "เลขที่ 2 ชั้น 3 อาคารเพลินจิตเซ็นเตอร์ \nถนนสุขุมวิท แขวงคลองเตย \nเขตคลองเตย กรุงเทพฯ 10110"
+
 const Footer = () => {
   return (
-    <div className='absolute bottom-0 h-70 w-screen bg-green-300 flex'>
-        <div>
-
+    <div className='absolute bottom-0 h-100 w-screen bg-(--footer-blue) flex'>
+        <div className='flex flex-row'>
+          <p className='text-white whitespace-pre-line'>{address}</p>
         </div>
     </div>
   )
