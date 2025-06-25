6244b33ce1d22d5c737074cb037aa77ddd9f8dd1
Riodsa <titan092549@gmail.com>
2025-06-25 10:53:10 +0700

    fix: fix variant error

--- src/variants/variant.tsx
+++ src/variants/variant.tsx
@@ -1,6 +1,6 @@
 export const FloatUp = {
     hidden: {y:50, opacity: 0},
-    visible: {y:0, opacity: 1, transition: {duration:0.4, ease:"easeOut"}},
+    visible: {y:0, opacity: 1, transition: {duration:0.4}},
     hover: {opacity: 1}
 }
 
